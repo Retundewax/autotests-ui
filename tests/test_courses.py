8f08cc312eb9da8715385916d85bbff9f7e54f03
Retundewax <KostikovDV@yandex.ru>
2026-04-07 01:40:10 +0300

    Pytest's fixtures

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,42 +1,18 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 import pytest
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as pl:
-        chrom = pl.chromium.launch(headless=False)
-        context = chrom.new_context()
-        page = context.new_page()
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+def test_empty_courses_list(chromium_page_with_state: Page):
+    courses_header = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_header).to_have_text('Courses')
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    folder_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(folder_icon).to_be_visible()
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+    result_header = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(result_header).to_have_text('There is no results')
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
+    description_line = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(description_line).to_have_text('Results from the load test pipeline will be displayed here')
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-        context.storage_state(path='browser-state.json')
-
-    with sync_playwright() as pl:
-        chrom = pl.chromium.launch(headless=False)
-        context = chrom.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        courses_header = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_header).to_have_text('Courses')
-
-        folder_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(folder_icon).to_be_visible()
-
-        result_header = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(result_header).to_have_text('There is no results')
-
-        description_line = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(description_line).to_have_text('Results from the load test pipeline will be displayed here')
